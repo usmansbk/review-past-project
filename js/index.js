c6f41e12bfa4a14dbd6299ec442c0f2556de8762
Usman <usmansbk@gmail.com>
2021-06-02 13:04:00 +0100

    Fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // !! IMPORTANT README:
-
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
-
+// You may add additional external JS and CSS as needed to complete the project,
+// however the current external resource MUST remain in place for the tests to work.
+// BABEL must also be left in place.
 /***********
 INSTRUCTIONS:
   - Select the project you would 
@@ -21,8 +21,10 @@ INSTRUCTIONS:
     you should get helpful errors 
     along the way!
     ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+/**
+ * PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..}
+ * or html {..}, or to all elements within body or html, i.e. h1 {..},
+ * has the potential to pollute the test suite's CSS. Try adding: * { color: red },
+ * for a quick example!
+ * Once you have read the above messages, you can delete all comments
+ */
